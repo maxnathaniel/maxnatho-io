428c5bfa9bd110717edd81aa904821c2bce5020b
Max Ho <maxhowl86@gmail.com>
2020-08-31 16:51:14 +0800

    updated blog template max-width

--- src/components/layout.js
+++ src/components/layout.js
@@ -58,7 +58,7 @@ class Layout extends React.Component {
         style={{
           marginLeft: `auto`,
           marginRight: `auto`,
-          maxWidth: rhythm(24),
+          maxWidth: rhythm(35),
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
       >
